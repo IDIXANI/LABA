b6db2e3ca87c4739ac2199ac67a06d5b3dc4f788
IDIXANI <118821261+IDIXANI@users.noreply.github.com>
2025-03-06 21:27:05 +0700

    Update LABA1.c

--- ConsoleApplication1/ConsoleApplication1/LABA1.c
+++ ConsoleApplication1/ConsoleApplication1/LABA1.c
@@ -5,14 +5,14 @@
 
 // Функции для треугольника
 void triangle(float a, float b, float c) {
-    if (a + b <= c  a + c <= b  b + c <= a) {
+    if (a + b <= c || a + c <= b || b + c <= a) {
         printf("\nОшибка: Такой треугольник не существует!\n");
         return;
     }
     float perimeter = a + b + c;
     float s = perimeter / 2;
     float area = sqrt(s * (s - a) * (s - b) * (s - c));
-    int isIsosceles = (a == b  a == c  b == c);
+    int isIsosceles = (a == b || a == c || b == c);
     printf("\nТреугольник:\nПериметр: %.2f\nПлощадь: %.2f\nРавнобедренный: %s\n", perimeter, area, isIsosceles ? "Да" : "Нет");
 }
 
