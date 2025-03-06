13b546084dabf72c404ea41df590d687a236694e
IDIXANI <118821261+IDIXANI@users.noreply.github.com>
2025-03-06 19:16:44 +0700

    Update LABA1.c

--- ConsoleApplication1/ConsoleApplication1/LABA1.c
+++ ConsoleApplication1/ConsoleApplication1/LABA1.c
@@ -3,14 +3,6 @@
 #include <stdio.h>
 #include <math.h>
 
-// Функции для прямоугольника
-void rectangle(float a, float b) {
-    float perimeter = 2 * (a + b);
-    float area = a * b;
-    float diagonal = sqrt(a * a + b * b);
-    printf("\nПрямоугольник:\nПериметр: %.2f\nПлощадь: %.2f\nДиагональ: %.2f\n", perimeter, area, diagonal);
-}
-
 // Функции для треугольника
 void triangle(float a, float b, float c) {
     if (a + b <= c  a + c <= b  b + c <= a) {
@@ -24,32 +16,14 @@ void triangle(float a, float b, float c) {
     printf("\nТреугольник:\nПериметр: %.2f\nПлощадь: %.2f\nРавнобедренный: %s\n", perimeter, area, isIsosceles ? "Да" : "Нет");
 }
 
-// Функции для трапеции
-void trapezoid(float a, float b, float c, float d, float h) {
-    float perimeter = a + b + c + d;
-    float area = ((a + b) / 2) * h;
-    float midLine = (a + b) / 2;
-    printf("\nТрапеция:\nПериметр: %.2f\nПлощадь: %.2f\nСредняя линия: %.2f\n", perimeter, area, midLine);
-}
-
 int main() {
-    setlocale(LC_ALL, "");
+    setlocale(LC_ALL, "Rus");
     float a, b, c, d, h;
-
-    // Ввод и расчет параметров для прямоугольника
-    printf("Введите стороны прямоугольника (a b): ");
-    scanf("%f %f", &a, &b);
-    rectangle(a, b);
 
     // Ввод и расчет параметров для треугольника
     printf("\nВведите стороны треугольника (a b c): ");
     scanf("%f %f %f", &a, &b, &c);
     triangle(a, b, c);
-
-    // Ввод и расчет параметров для трапеции
-    printf("\nВведите основания и боковые стороны трапеции (a b c d) и высоту h: ");
-    scanf("%f %f %f %f %f", &a, &b, &c, &d, &h);
-    trapezoid(a, b, c, d, h);
 
     return 0;
 }
