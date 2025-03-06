2499b93528bde185eeeee7fea8dd07adf1821ff1
IDIXANI <118821261+IDIXANI@users.noreply.github.com>
2025-03-06 19:13:51 +0700

    Update LABA1.c

--- ConsoleApplication1/ConsoleApplication1/LABA1.c
+++ ConsoleApplication1/ConsoleApplication1/LABA1.c
@@ -3,21 +3,20 @@
 #include <stdio.h>
 #include <math.h>
 
-
-// Функции для трапеции
+// Р¤СѓРЅРєС†РёРё РґР»СЏ С‚СЂР°РїРµС†РёРё
 void trapezoid(float a, float b, float c, float d, float h) {
     float perimeter = a + b + c + d;
     float area = ((a + b) / 2) * h;
     float midLine = (a + b) / 2;
-    printf("\nТрапеция:\nПериметр: %.2f\nПлощадь: %.2f\nСредняя линия: %.2f\n", perimeter, area, midLine);
+    printf("\nРўСЂР°РїРµС†РёСЏ:\nРџРµСЂРёРјРµС‚СЂ: %.2f\nРџР»РѕС‰Р°РґСЊ: %.2f\nРЎСЂРµРґРЅСЏСЏ Р»РёРЅРёСЏ: %.2f\n", perimeter, area, midLine);
 }
 
 int main() {
     setlocale(LC_ALL, "");
     float a, b, c, d, h;
 
-    // Ввод и расчет параметров для трапеции
-    printf("\nВведите основания и боковые стороны трапеции (a b c d) и высоту h: ");
+    // Р’РІРѕРґ Рё СЂР°СЃС‡РµС‚ РїР°СЂР°РјРµС‚СЂРѕРІ РґР»СЏ С‚СЂР°РїРµС†РёРё
+    printf("\nР’РІРµРґРёС‚Рµ РѕСЃРЅРѕРІР°РЅРёСЏ Рё Р±РѕРєРѕРІС‹Рµ СЃС‚РѕСЂРѕРЅС‹ С‚СЂР°РїРµС†РёРё (a b c d) Рё РІС‹СЃРѕС‚Сѓ h: ");
     scanf("%f %f %f %f %f", &a, &b, &c, &d, &h);
     trapezoid(a, b, c, d, h);
 
