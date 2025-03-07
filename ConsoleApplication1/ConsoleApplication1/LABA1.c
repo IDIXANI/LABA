d906334d7396d8daa99065d8398f118d8a9caf6b
egorka952 <egorpaskov619@gmail.com>
2025-03-07 10:12:54 +0700

    Update LABA1.c

--- ConsoleApplication1/ConsoleApplication1/LABA1.c
+++ ConsoleApplication1/ConsoleApplication1/LABA1.c
@@ -5,25 +5,30 @@
 
 // Функции для треугольника
 void triangle(float a, float b, float c) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+        printf("\nОшибка: стороны треугольника должны быть положительными!\n");
+        return;
+    }
+
     if (a + b <= c || a + c <= b || b + c <= a) {
         printf("\nОшибка: Такой треугольник не существует!\n");
         return;
     }
+
     float perimeter = a + b + c;
     float s = perimeter / 2;
     float area = sqrt(s * (s - a) * (s - b) * (s - c));
-    int isIsosceles = (a == b || a == c || b == c);
-    printf("\nТреугольник:\nПериметр: %.2f\nПлощадь: %.2f\nРавнобедренный: %s\n", perimeter, area, isIsosceles ? "Да" : "Нет");
+    int rovnobedr = (a == b || a == c || b == c);
+    printf("\nТреугольник:\nПериметр: %.2f\nПлощадь: %.2f\nРавнобедренный: %s\n", perimeter, area, rovnobedr ? "Да" : "Нет");
 }
 
 int main() {
-    setlocale(LC_ALL, "Rus");
-    float a, b, c, d, h;
+    setlocale(LC_ALL, "");
+    float a, b, c, d;
 
     // Ввод и расчет параметров для треугольника
     printf("\nВведите стороны треугольника (a b c): ");
     scanf("%f %f %f", &a, &b, &c);
     triangle(a, b, c);
-
     return 0;
 }
