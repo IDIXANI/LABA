696429f88d64ffaaea43dafb0ca0d12f45479d7e
egorka952 <egorpaskov619@gmail.com>
2025-03-07 10:12:07 +0700

    Update LABA1.c

--- ConsoleApplication1/ConsoleApplication1/LABA1.c
+++ ConsoleApplication1/ConsoleApplication1/LABA1.c
@@ -4,21 +4,28 @@
 #include <math.h>
 
 // Функции для трапеции
-void trapezoid(float a, float b, float c, float d, float h) {
+void trapezoid(float a, float b, float c, float d) {
+    if (a <= 0 || b <= 0 || c <= 0 || d <= 0) {
+        printf("\nОшибка: стороны и высота трапеции должны быть положительными!\n");
+        return;
+    }
+
     float perimeter = a + b + c + d;
-    float area = ((a + b) / 2) * h;
+    float pp = perimeter / 2;
+    float area = sqrt((pp - a) * (pp - b) * (pp - c) * (pp - d));
     float midLine = (a + b) / 2;
+ 
     printf("\nТрапеция:\nПериметр: %.2f\nПлощадь: %.2f\nСредняя линия: %.2f\n", perimeter, area, midLine);
 }
 
 int main() {
     setlocale(LC_ALL, "Rus");
-    float a, b, c, d, h;
-
+    float a, b, c, d;
+    
     // Ввод и расчет параметров для трапеции
-    printf("\nВведите основания и боковые стороны трапеции (a b c d) и высоту h: ");
-    scanf("%f %f %f %f %f", &a, &b, &c, &d, &h);
-    trapezoid(a, b, c, d, h);
+    printf("\nВведите основания и боковые стороны трапеции (a b c d): ");
+    scanf("%f %f %f %f", &a, &c, &b, &d);
+    trapezoid(a, c, b, d);
 
     return 0;
 }
